dfb2682f76f725ff0bc848a86294cf35364c5e08
0xIbuki <junta8918@gmail.com>
2023-03-17 12:19:55 +0900

    use API routes and restructure

--- backend/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-git init
-heroku git:remote -a $APP_NAME
-heroku buildpacks:set heroku/nodejs -a $APP_NAME
-git add .
-git commit -am "publish back"
-git push heroku main --force
-rm -rf .git
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,24 +1,33 @@
-import express from "express";
-import cors from "cors";
-import { DataRequest, ZkConnect, ZkConnectServerConfig } from "@sismo-core/zk-connect-server";
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import {
+  DataRequest,
+  ZkConnect,
+  ZkConnectServerConfig,
+} from "@sismo-core/zk-connect-server";
 
 const emailMemoryStore = new Map();
 
 const zkConnectConfig: ZkConnectServerConfig = {
   appId: "0x112a692a2005259c25f6094161007967",
-}
+};
 const zkConnect = ZkConnect(zkConnectConfig);
 
 const THE_MERGE_CONTRIBUTOR = DataRequest({
   groupId: "0x42c768bb8ae79e4c5c05d3b51a4ec74a",
 });
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+type Data = {
+  email: string;
+};
 
-app.post("/subscribe", async (req, res) => {
+export default async function handler(
+  req: NextApiRequest,
+  // res: NextApiResponse<Data>
+  res: NextApiResponse
+) {
   const { email, zkConnectResponse } = req.body;
+
   try {
     const { vaultId } = await zkConnect.verify(zkConnectResponse, {
       dataRequest: THE_MERGE_CONTRIBUTOR,
@@ -28,26 +37,19 @@ app.post("/subscribe", async (req, res) => {
     if (!email) {
       if (emailMemoryStore.has(vaultId)) {
         const existingEmail = emailMemoryStore.get(vaultId);
-        res.status(200).send({
+        res.status(200).json({
           email: existingEmail,
           vaultId,
           status: "already-subscribed",
         });
         return;
       }
-      res.status(200).send({ status: "not-subscribed", vaultId });
+      res.status(200).json({ status: "not-subscribed", vaultId });
     } else {
       emailMemoryStore.set(vaultId, email);
-      res.status(200).send({ email, status: "success", vaultId });
+      res.status(200).json({ email, status: "success", vaultId });
     }
   } catch (e: any) {
-    res.status(400).send({ status: "error", message: e.message });
+    res.status(400).json({ status: "error", message: e.message });
   }
-});
-
-app.post("/reset", async (req, res) => {
-  emailMemoryStore.clear();
-  res.status(200).send({ status: "success" });
-});
-
-app.listen(process.env.PORT ?? 8080);
+}
